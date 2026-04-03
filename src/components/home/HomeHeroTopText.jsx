67a7dd1b10d4aea2262b6e9af00ac13c83165c37
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-03 18:17:23 +0500

    hero top section

--- src/components/home/HomeHeroTopText.jsx
+++ src/components/home/HomeHeroTopText.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Video from "./Video";
 
 const HomeHeroTopText = () => {
   return (
-    <div>HomeHeroTopText</div>
-  )
-}
+    <div className="p-3">
+      <h1 className="w-full text-[9.5vw] uppercase leading-[0.87] text-center py-0 px-[10vw] grow ">
+        <div className="pt-2">
+          <span>The spark for</span>
+        </div>
+        <div className="pt-2 flex justify-center items-baseline">
+          <span>all</span>
+          <div className="h-24 w-52 aspect-video rounded-full overflow-hidden isolate">
+            <div className="w-full h-full rounded-full overflow-hidden">
+              <Video />
+            </div>
+          </div>
+          <span>things</span>
+        </div>
+        <div className="pt-2">
+          <span>creative</span>
+        </div>
+      </h1>
+    </div>
+  );
+};
 
-export default HomeHeroTopText
+export default HomeHeroTopText;
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -6,8 +6,7 @@ const Video = () => {
         src="https://player.vimeo.com/progressive_redirect/playback/1119600858/rendition/1080p/file.mp4?loc=external&amp;log_user=0&amp;signature=c4a137161d6ce80a52c50c7ee23d4fdf8df103bfc816252fc304e317a43bacc6"
         muted
         autoPlay
-        data-monitor="source"
-        data-home="video"
+        loop
       ></video>
     </div>
   );
