a90b5d29c7a632c01a21e0d18d16800f78403416
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-03 16:36:50 +0500

    video on homepage

---
+++ src/components/home/HomeHeroTopText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeHeroTopText = () => {
+  return (
+    <div>HomeHeroTopText</div>
+  )
+}
+
+export default HomeHeroTopText
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Video from "../components/home/Video";
+import HomeHeroTopText from "../components/home/HomeHeroTopText";
+import HomeHeroBottomText from "../components/home/HomeHeroBottomText";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="h-screen w-screen fixed">
+        <Video />
+      </div>
+      <div className="h-screen w-screen relative flex flex-col">
+        <HomeHeroTopText />
+        <HomeHeroBottomText />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
