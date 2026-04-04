b98734fe6ff3d23089bf8c3dabf8816bb6a2a451
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-04 22:42:31 +0500

    images

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,62 +1,60 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
 const Agency = () => {
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const imageArray = [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/Olivier_640X960.jpg?w=640&h=960&s=9b715851e76624b3d00c6715af5c47cd",
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+  ];
+
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        start: "top 20%",
+        end: "top -180%",
+        scrub: true,
+        pin: true,
+        onUpdate: function () {
+          console.log("hurray");
+        },
+      },
+    });
+  });
   return (
     <div className="bg-white text-black">
       <div className="section1">
-        <div className="absolute h-80 w-60 rounded-3xl overflow-hidden top-96 left-[30vw]">
+        <div
+          ref={imageDivRef}
+          className="absolute h-70 w-50 rounded-3xl overflow-hidden top-25 left-[30vw]"
+        >
           <img
+            ref={imageRef}
             className="w-full h-full object-cover"
             src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
             alt="Carl"
           />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/Olivier_640X960.jpg?w=640&h=960&s=9b715851e76624b3d00c6715af5c47cd"
-            alt="Oliver"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991"
-            alt="ChantalG"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2"
-            alt="Michele"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6"
-            alt="Mel"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a"
-            alt="Camille"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847"
-            alt="Meggie"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6"
-            alt="Joel"
-          />
-          <img
-            className="w-full h-full object-cover"
-            src="https://k72.ca/images/teamMembers/blank.jpg?w=640&h=960&s=252a0cd13e881c6a268fe3c75f8abbca"
-            alt="blank"
-          />
         </div>
-        <div className="relative font-[lausanne-regular]">
-          <div className="mt-[55vh]">
-            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+        <div className="relative">
+          <div className="pt-[55vh]">
+            <h2 className="text-[20vw] font-[lausanne-regular] text-center uppercase leading-[18vw]">
               SEVEN7Y <br /> TWO
-            </h1>
+            </h2>
           </div>
           <div className="pl-[40.2%] mt-20">
-            <p className="text-6xl indent-50">
+            <p className="text-6xl indent-50 font-[lausanne-light]">
               We’re inquisitive and open-minded, and we make sure creativity
               crowds out ego from every corner. A brand is a living thing, with
               values, a personality and a story. If we ignore that, we can
