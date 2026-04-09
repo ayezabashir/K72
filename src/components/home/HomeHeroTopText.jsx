13a30a7d77759ea2ac9077e196d555736ac93bc0
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-09 16:50:38 +0500

    ellipse animation on homepage

--- src/components/home/HomeHeroTopText.jsx
+++ src/components/home/HomeHeroTopText.jsx
@@ -1,6 +1,33 @@
+import { useRef } from "react";
 import Video from "./Video";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const HomeHeroTopText = () => {
+  const circleRef = useRef(null);
+  const ellipseRef = useRef(null);
+  useGSAP(() => {
+    const ellipse = ellipseRef.current;
+    const length = ellipse.getTotalLength();
+
+    gsap.set(ellipse, {
+      strokeDasharray: length,
+      strokeDashoffset: length,
+    });
+
+    const tl = gsap.timeline({repeat: -1, repeatDelay:1});
+    tl.to(ellipse, {
+      strokeDashoffset: 0,
+      duration: 1.5,
+      ease: "power2.inOut"
+    })
+    .to(circleRef.current,{
+      scale: 1.05,
+      duration: 0.3,
+      yoyo: true,
+      repeat:1,
+    })
+  },{scope: circleRef});
   return (
     <div>
       <h1 className="w-full text-[9.5vw] uppercase leading-[0.87] text-center py-0 px-[10vw] grow ">
@@ -16,7 +43,22 @@ const HomeHeroTopText = () => {
           </div>
           <span>things</span>
         </div>
-        <div className="pt-2">
+        <div className="pt-2 circle relative">
+          <svg
+            ref={circleRef}
+            className="absolute top-0 left-0 w-full h-full pointer-events-none"
+            viewBox="0 0 573.46 105.38"
+          >
+            <ellipse
+              ref={ellipseRef}
+              cx="286.73"
+              cy="52.69"
+              rx="284.73"
+              ry="50.69"
+              className="stroke-[#d3fd50] stroke-2 fill-transparent"
+              style={{ strokeLinecap: "round" }}
+            ></ellipse>
+          </svg>
           <span>creative</span>
         </div>
       </h1>
