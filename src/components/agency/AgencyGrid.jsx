fa8f47f8351654ded8eb3f60aead529502d76ecc
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-10 14:58:50 +0500

    scroll triger bg effect

--- src/components/agency/AgencyGrid.jsx
+++ src/components/agency/AgencyGrid.jsx
@@ -5,19 +5,19 @@ const AgencyGrid = () => {
   return (
     <div className="px-[9vw] my-20 grid grid-cols-3 grid-rows-2 gap-[5vw]">
       <div>
-        <p className="font-[lausanne-regular] text-lg">{expertise.title}</p>
+        <p className="font-[lausanne-regular] text-lg mix-blend-difference text-white">{expertise.title}</p>
       </div>
       <div>
         <ul>
           {expertise.list.map((li, index) => (
-            <li className="font-[lausanne-regular] text-lg" key={index}>{li}</li>
+            <li className="font-[lausanne-regular] text-lg mix-blend-difference text-white" key={index}>{li}</li>
           ))}
         </ul>
       </div>
       <div></div>
       {missionStatements.map((item, index) => (
         <div key={index}>
-          <p className="font-[lausanne-regular] text-lg">
+          <p className="font-[lausanne-regular] text-lg mix-blend-difference text-white">
             {item.label}_{item.text}
           </p>
         </div>
--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,13 +1,43 @@
+import { useGSAP } from "@gsap/react";
 import AgencyGrid from "../components/agency/AgencyGrid";
 import HeroContent from "../components/agency/HeroContent";
 import MarqueeContent from "../components/agency/MarqueeContent";
+import { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Agency = () => {
+  const mainWrapperRef = useRef(null);
+  const marqueeRef = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.to(mainWrapperRef.current, {
+        backgroundColor: "#000000",
+        scrollTrigger: {
+          trigger: marqueeRef.current,
+          start: "top 80%",
+          end: "top 20%",
+          scrub: 1,
+        },
+      });
+    },
+    { scope: mainWrapperRef },
+  );
+
   return (
-    <>
+    <div
+      ref={mainWrapperRef}
+      className="bg-white transition-colors duration-500"
+    >
       <HeroContent />
       <AgencyGrid />
-      <div className="bg-black min-h-screen py-50 w-full overflow-x-hidden">
+      <div
+        ref={marqueeRef}
+        className=" min-h-screen py-50 w-full overflow-x-hidden"
+      >
         <MarqueeContent
           bg_text1="MARIE-PIER"
           bg_text2="DAIGLE"
@@ -21,7 +51,7 @@ const Agency = () => {
           center_img="https://k72.ca/images/teamMembers/MEGGIE_640X980_2.jpg?w=640&h=960&s=7d78cdb1fad347408e05a311cc4018ef"
         />
       </div>
-    </>
+    </div>
   );
 };
 
