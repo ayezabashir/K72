be4aa18cc5cf9c455c558860bcbbe325d0f0958d
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-13 17:43:52 +0500

    agency project

--- src/components/agency/AgencyProjects.jsx
+++ src/components/agency/AgencyProjects.jsx
@@ -1,22 +1,44 @@
+import gsap from "gsap";
 import { projects } from "./data";
 import Project from "./Project";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const AgencyProjects = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    const projectsEl = gsap.utils.toArray(".project-card");
+    projectsEl.forEach((project) => {
+      ScrollTrigger.create({
+        trigger: project,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+      });
+    });
+  });
+
   return (
-    <div className="py-40 relative">
+    <div ref={containerRef} className="py-40 relative">
       <div className="absolute top-47.5 z-10 w-full">
-        <h4 className="uppercase text-2xl font-[lausanne-regular] cursor-pointer text-center text-white hover:text-[#D3FD50]">
+        <h4 className="uppercase text-2xl font-[lausanne-regular] cursor-pointer text-center text-white hover:text-[#D3FD50] hover:uppercase">
           view all projects
         </h4>
       </div>
-      {projects.map((project, index) => (
-        <Project
-          key={index}
-          bg_img={project.img}
-          title={project.title}
-          subtitle={project.subtitle}
-        />
-      ))}
+      <div className="project-card w-full h-screen">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            bg_img={project.img}
+            title={project.title}
+            subtitle={project.subtitle}
+          />
+        ))}
+      </div>
     </div>
   );
 };
