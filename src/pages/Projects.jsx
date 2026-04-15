d8b6ce8079a17eec48475a45f52a72e4875bab44
Ayeza Bashir <ayezabashir46@gmail.com>
2026-04-15 18:48:55 +0500

    project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 import { createPortal } from "react-dom";
 import Works from "../components/projects/Works";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Projects = () => {
   const [hoveredProject, setHoveredProject] = useState(null);
+  useGSAP(() => {
+    if (hoveredProject) {
+      gsap.fromTo(
+        ".capitalize",
+        {
+          textDecorationLine: "line-through",
+          textDecorationColor: "#d3fd50",
+        },
+        {
+          textDecorationLine: "none",
+          duration: 0.6,
+          stagger: 0.1,
+          delay: 0.2,
+          ease: "power2.inOut",
+        },
+      );
+    }
+  }, [hoveredProject]);
 
   return (
     <div className="bg-white ">
       {createPortal(
         <div
-          className={`fixed top-0 left-0 w-full h-15 bg-white text-black z-[9999] transition-opacity duration-300 border-y border-black flex justify-between items-center p-4 ${
+          className={`fixed top-0 left-0 w-full h-15 bg-white text-black z-99 transition-opacity duration-300 border-y border-black flex justify-between items-center p-4 ${
             hoveredProject ? "opacity-100" : "opacity-0 pointer-events-none"
           }`}
         >
